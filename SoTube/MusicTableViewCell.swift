0aef80d439ad25a56e84285481509af4f2573950
Yoni Celen <yoni.celen@hotmail.com>
2017-05-09 15:42:41 +0200

    added MusicViews added Controllers -> MusicControllers

---
+++ SoTube/MusicTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MusicTableViewCell.swift
+//  SoTube
+//
+//  Created by VDAB Cursist on 09/05/17.
+//  Copyright © 2017 NV Met Talent. All rights reserved.
+//
+
+import UIKit
+
+class MusicTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
