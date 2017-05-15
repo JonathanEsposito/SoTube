54ba6582056500f04301072d397c168dca4244ba
Yoni Celen <yoni.celen@hotmail.com>
2017-05-15 15:57:44 +0200

    updated payment

--- SoTube/Controllers/PaymentViewController.swift
+++ SoTube/Controllers/PaymentViewController.swift
@@ -17,8 +17,19 @@ class PaymentViewController: UIViewController {
     }
     
     @IBAction func buySoCoin(_ sender: UIButton) {
-        let paymentVC = paymentViewModel.giveTransactionVC(forProduct: "TestItems", amount: Int(sender.title(for: .normal)!)!, inCurrency: .euro, withSku: nil)
-        present(paymentVC!, animated: true, completion: nil)
+        let paymentVC = paymentViewModel.giveTransactionVC(forProduct: "SoCoins", amount: Int(sender.title(for: .normal)!)!, inCurrency: .euro, withSku: nil)
+        
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let payWithPayPal = UIAlertAction(title: "PayPal", style: UIAlertActionStyle.default, handler: {(alert :UIAlertAction!) in
+            self.present(paymentVC!, animated: true, completion: nil)
+        })
+        
+        let cancelAction = UIAlertAction(title: "cancel", style: UIAlertActionStyle.cancel/*.default*/, handler: {(alert :UIAlertAction!) in
+            print("Cancel button tapped")
+        })
+        alertController.addAction(cancelAction)
+        alertController.addAction(payWithPayPal)
+        present(alertController, animated: true, completion: nil)
     }
-    
 }
